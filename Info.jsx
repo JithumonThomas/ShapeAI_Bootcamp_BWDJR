3bb9428e187926c07a7c19726e2e55d08f929ad3
JithumonThomas <75689968+JithumonThomas@users.noreply.github.com>
2021-06-26 20:30:46 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+function Info() {
+  return (
+    <div>
+      <div className="note">
+        <h1>Details</h1>
+        <p>
+          My Name Is JIthumon Thomas, I am Studying Diploma In Information
+          Technology
+        </p>
+      </div>
+      <div className="note">
+        <h1>College</h1>
+        <p>Government Polytechnic College, Kerala Idukki Purappuzha </p>
+      </div>
+      <div className="note">
+        <h1>Skills</h1>
+        <p>Javascript , Php , Mysql , C , Java , Basics Of Python </p>
+      </div>
+      <div className="note">
+        <h1>First Website</h1>
+        <p>This Is My First React Website.</p>
+      </div>
+    </div>
+  );
+}
+export default Info;
